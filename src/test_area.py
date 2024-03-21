46f08f279292e5cb65bc6fbda9ef20a145e6797f
tej161097 <158339867+tej161097@users.noreply.github.com>
2024-03-21 12:43:45 -0400

    Add files via upload

---
+++ src/test_area.py
@@ -0,0 +1,14 @@
+import pytest  
+from src.area import calculate_area_square  
+  
+def test_calculate_area_square_negative():  
+    with pytest.raises(TypeError):  
+        calculate_area_square(-2)  
+  
+def test_calculate_area_square_string():  
+    with pytest.raises(TypeError):  
+        calculate_area_square("2")  
+  
+def test_calculate_area_square_list():  
+    with pytest.raises(TypeError):  
+        calculate_area_square([2])
